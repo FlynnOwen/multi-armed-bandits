c7dd3212420caf7b98660f5d84189f3dbe139610
Flynn Owen <flynnoo49@gmail.com>
2023-11-05 14:11:15 +1300

    add generate method, fix unit tests to use estimated value, rather than true

--- src/bandit.py
+++ src/bandit.py
@@ -1,20 +1,41 @@
+from random import randint
 from dataclasses import dataclass, field
 
 
 @dataclass
 class Bandit:
-    """Single bandit."""
+    """Single bandit, simulating over a Bernoulli distribution."""
 
     true_parameter: float
     _results: list[int] = field(init=False)
 
+    def generate(self):
+        """
+        Generates a single result from the pull of the
+        armed bandit.
+        """
+        result = randint(0, 1)
+        self._results.append(result)
+
+        return result
+
     @property
     def parameter_hat(self):
-        return self.true_parameter
+        """
+        The estimated parameter for this bandit.
+        Note that the Maximum Likelihood estimator is used.
+        """
+        return sum(self._results)/len(self._results)
 
     @property
     def simulations(self):
-        pass
+        """
+        Number of simulations that this armed bandit has performed.
+        """
+        return len(self._results)
+
+    def __len__(self):
+        return len(self._results)
 
     def __eq__(self, value: float):
         return self.parameter_hat == value
--- src/tests/test_bandit.py
+++ src/tests/test_bandit.py
@@ -4,29 +4,41 @@ from src.bandit import Bandit
 
 
 @pytest.fixture
+def mock_parameter_hat():
+    return 0.5
+
+
+@pytest.fixture
 def true_parameter():
     return 0.5
 
+
 @pytest.fixture
-def bandit(true_parameter):
-    return Bandit(true_parameter)
+def bandit(true_parameter, mock_parameter_hat):
+    bandit = Bandit(true_parameter)
+    
+    # TODO: MonkeyPatch parameter_hat, rather than _results.
+    bandit._results = [mock_parameter_hat]
+
+    return bandit
 
 
-def test_eq(bandit, true_parameter):
-    assert bandit == true_parameter
+
+def test_eq(bandit, mock_parameter_hat):
+    assert bandit == mock_parameter_hat
 
 
-def test_le(bandit, true_parameter):
-    assert bandit <= true_parameter and bandit < true_parameter + 0.1
+def test_le(bandit, mock_parameter_hat):
+    assert bandit <= mock_parameter_hat and bandit < mock_parameter_hat + 0.1
 
 
-def test_lt(bandit, true_parameter):
-    assert bandit < true_parameter + 0.1
+def test_lt(bandit, mock_parameter_hat):
+    assert bandit < mock_parameter_hat + 0.1
 
 
-def test_ge(bandit, true_parameter):
-    assert bandit >= true_parameter and bandit >= true_parameter - 0.1
+def test_ge(bandit, mock_parameter_hat):
+    assert bandit >= mock_parameter_hat and bandit >= mock_parameter_hat - 0.1
 
 
-def test_gt(bandit, true_parameter):
-    assert bandit > true_parameter - 0.1
+def test_gt(bandit, mock_parameter_hat):
+    assert bandit > mock_parameter_hat - 0.1
