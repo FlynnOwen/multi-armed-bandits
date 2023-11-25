94663f1ff8fc9fd4197c9432a0745ba657efdbe6
Flynn Owen <flynnoo49@gmail.com>
2023-11-25 17:55:44 +1300

    creation of utils.py

---
+++ src/utils/utils.py
@@ -0,0 +1,19 @@
+from math import sqrt, log
+
+
+def ucb(q: float,
+        t: int,
+        c: float,
+        q_t: int):
+    """
+    Calculates the upper confidence bound.
+
+    Where UCB = Argmax(a) Q_t(a) + c(sqrt((ln(t)/N_t(a))))
+
+    Note that:
+    q: Average reward for the best bandit.
+    t: Number of total bandit pulls.
+    c: A constant that balances exploration vs exploitation.
+    q_t: The number of times the best bandit has been pulled.
+    """
+    return q + (c * sqrt(log(t)/q_t))
