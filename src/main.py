6974818cef12b3aec0d493fd4c70cdde7954dd50
Flynn Owen <flynnoo49@gmail.com>
2024-02-11 11:50:39 +1300

    add typer args and Enums

--- src/main.py
+++ src/main.py
@@ -1,23 +1,40 @@
-import typer
+from typing import Annotated
+from enum import Enum
+
+import typer # noqa: F401
+
+from src.bandit import BernoulliBandit, BanditCollection # noqa: F401
+from src.metrics import Metrics # noqa: F401
+from src.simulation import (EpsilonFirstStrategy, # noqa: F401
+                            EpsilonDecreasingStrategy, # noqa: F401
+                            EpsilonGreedyStrategy) # noqa: F401
 
 app = typer.Typer()
 
+class Distribution(str, Enum):
+    bernoulli = "bernoulli"
+    gaussian = "gaussian"
+
+
+class Strategy(str, Enum):
+    epsilon_first = "epsilon_first"
+    epsilon_decreasing = "epsilon_decreasing"
+    epsilon_greedy = "epsilon_greedy"
+
 
 @app.command()
-def simulate(distirbution: str,
-             num_simulations: int,
-             num_bandits: int,
-             parameter_distribution: str,
-             serialize_file_path: str,
-             print_results: bool,
-             print_plots: bool) -> None:
+def simulate(strategy: Strategy = Strategy.epsilon_greedy.value,
+             distirbution: Distribution = Distribution.bernoulli.value,
+             num_simulations: Annotated[int, typer.Option(min=10)] = 500,
+             num_bandits: Annotated[int, typer.Option(min=2)] = 10,
+             print_metrics: bool = False,
+             print_plots: bool = False,
+             **kwargs) -> None: # noqa: ANN003
     """
-    Placeholder for the main function.
-
-    Current arguments are ideas only.
+    Run a multi-armed bandit simulation.
     """
     pass  #noqa: PIE790
 
 
 if __name__ == "__main__":
-    pass
+    typer.run(simulate)
