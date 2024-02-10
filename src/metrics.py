ffe0d75040d1c7124647ae0e9412531164632ea6
Flynn Owen <flynnoo49@gmail.com>
2024-02-10 22:56:38 +1300

    two additional plots

--- src/metrics.py
+++ src/metrics.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
 
-from tabulate import tabulate
-import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import seaborn as sns
+from tabulate import tabulate
 
 from src.simulation import SemiUniformStrategy
 
@@ -68,10 +69,66 @@ class Metrics:
              numalign="left"
         )
 
-    def plots(self) -> None:
+    def residual_barplots(self) -> None:
         """
-        Generates plots to stdout of the multiarmed
-        bandit simulation process.
+        Barplots showing estimated vs true Parameter
+        values for each bandit.
+        """
+        parameters = [bandit.parameter_hat
+                      for bandit
+                      in self.bandit_collection.bandits] +\
+                      [bandit.true_parameter
+                       for bandit
+                       in self.bandit_collection.bandits]
+
+        parameter_types = ["Estimated"
+                           for _
+                           in range(len(self.bandit_collection))] +\
+                           ["True"
+                            for _
+                            in range(len(self.bandit_collection))]
+
+        bandit_id = list(range(1, len(self.bandit_collection.bandits) + 1)) * 2
+
+        data = pd.DataFrame({"Parameter Value": parameters,
+                            "Parameter Type": parameter_types,
+                            "Bandit ID": bandit_id})
+
+        sns.set_theme(style="whitegrid")
+        ax = sns.catplot(data=data,
+                 x="Bandit ID",
+                 y="Parameter Value",
+                 hue="Parameter Type",
+                 kind="bar",
+                 alpha=0.8)
+        ax.despine(left=True)
+        ax.set(title="Simulation Estimated vs True Parameters")
+
+    def pull_residual_scatterplot(self) -> None:
+        """
+        Scatterplot showing number of bandit pulls vs
+        the absolute residual for each bandit.
+        """
+        simulation_counts = [len(bandit)
+                             for bandit
+                             in self.bandit_collection]
+        abs_residuals = [abs(bandit.residual)
+                         for bandit in
+                         self.bandit_collection]
+        sim_counts_residuals = pd.DataFrame({
+            "Number of Pulls": simulation_counts,
+            "Absolute Resdidual": abs_residuals})
+
+        ax = sns.scatterplot(color="r",
+                     data=sim_counts_residuals,
+                     x="Absolute Resdidual",
+                     y="Number of Pulls")
+        ax.set(title="Number of Bandit Pulls vs Absolute Residual of Bandit")
+
+    def reward_timeseries_plot(self) -> None:
+        """
+        Generates a timeseries plot of the average reward
+        recieved during each stage of the simulation process.
         """
         sns.set_style("darkgrid")
         ax = sns.lineplot(data=self.average_reward_timeseries)
@@ -91,3 +148,12 @@ class Metrics:
 
         plt.legend()
         plt.show()
+
+    def generate_plots(self) -> None:
+        """
+        Generates plots to stdout of the multiarmed
+        bandit simulation process.
+        """
+        self.residual_barplots()
+        self.pull_residual_scatterplot()
+        self.reward_timeseries_plot()
